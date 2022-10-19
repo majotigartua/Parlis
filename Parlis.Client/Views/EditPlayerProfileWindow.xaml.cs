40e6876d184fb876536a632aef0bf40081f840c1
María José Torres Igartua <majotigartua@gmail.com>
2022-10-19 07:33:41 -0500

    Add the edit player profile window.

---
+++ Parlis.Client/Views/EditPlayerProfileWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+
+namespace Parlis.Client.Views
+{
+    public partial class EditPlayerProfileWindow : Window
+    {
+        public EditPlayerProfileWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ProfilePictureMouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+        }
+
+        private void AcceptButtonClick(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void ConfirmPlayerProfileButtonClick(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void DeletePlayerProfileClick(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void CancelButtonClick(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
