356b1dea2f5f01c6ce3acfacd9b62e3ee1e0bc34
sebastianbello <sebtrejo@outlook.es>
2022-10-19 20:12:45 -0500

    Update RecoverPasswordEmailAddressWindow.xaml.cs and RecoverPasswordWindow.xaml.cs files.

--- Parlis.Client/Views/RecoverPasswordEmailAddressWindow.xaml.cs
+++ Parlis.Client/Views/RecoverPasswordEmailAddressWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using Parlis.Client.Resources;
+using System.Windows;
 
 namespace Parlis.Client.Views
 {
@@ -7,6 +8,35 @@ namespace Parlis.Client.Views
         public RecoverPasswordEmailAddressWindow()
         {
             InitializeComponent();
+        }
+
+        private void RecoverPasswordButtonClick(object sender, RoutedEventArgs e)
+        {
+            var emailAddress = EmailAddressTextBox.Text;
+            if(!string.IsNullOrEmpty(emailAddress))
+            {
+                if(Utilities.ValidateEmailAddressFormat(emailAddress))
+                {
+                    var recoverPasswordWindow = new RecoverPasswordWindow();
+                    this.Visibility = Visibility.Hidden;
+                    recoverPasswordWindow.Show();
+                }
+                else
+                {
+                    MessageBox.Show(Properties.Resources.CHECK_ENTERED_INFORMATION_LABEL,
+                        Properties.Resources.INVALID_DATA_WINDOW_TITLE);
+                }
+            } 
+            else
+            {
+                MessageBox.Show(Properties.Resources.CHECK_ENTERED_INFORMATION_LABEL,
+                    Properties.Resources.EMPTY_FIELDS_WINDOW_TITLE);
+            }
+        }
+
+        private void CancelButtonClick(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
